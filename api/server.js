9d72e88bb68726f78accf77ba6c915ea9327deb3
Emmanuel Gonzalez <dev.egonzalez1@gmail.com>
2021-08-30 18:01:10 -0500

    Adds post user endpoint

--- api/server.js
+++ api/server.js
@@ -1,3 +1,27 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+// Import
+const express = require('express')
+
+const server = express()
+
+server.use(express.json()) // Teaches express to read JSON
+
+// Endpoints
+// Creates a user using the information sent inside the request body.
+server.post('/api/users', (req, res) => {
+  const newUser = req.body
+  User.create(newUser)
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ message: "error" })
+    })
+})
+
+
+
+//module.exports = {}; // EXPORT YOUR SERVER instead of {}
+module.exports = server
