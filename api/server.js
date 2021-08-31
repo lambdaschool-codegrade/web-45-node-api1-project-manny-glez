5ea3ed41c10de17556fc9ca0e11f83723351d4b0
Emmanuel Gonzalez <dev.egonzalez1@gmail.com>
2021-08-30 20:43:06 -0500

    Adds delete endpoint

--- api/server.js
+++ api/server.js
@@ -64,6 +64,7 @@ server.get('/api/users/:id', (req, res) => {
     })
 })
 
+// [DELETE] Removes the user with the specified id and returns the deleted user.
 server.delete('/api/users/:id', async (req, res) => {
   try {
     const possibleUser = await User.findById(req.params.id)
@@ -78,6 +79,33 @@ server.delete('/api/users/:id', async (req, res) => {
   } catch (err) {
     res.status(500).json({
       message: 'error deleting user',
+      err: err.message,
+      stack: err.stack,
+    })
+  }
+})
+
+// [PUT] Updates the user with the specified id using data from the request body. Returns the modified user
+server.put('/api/users/:id', async (req, res) => {
+  try {
+    const possibleUser = await User.findById(req.params.id)
+    if (!possibleUser) {
+      res.status(404).json({
+        message: 'The user with the right ID does not exist',
+      })
+    } else {
+      if(!req.body.name || !req.body.bio) {
+        res.status(400).json({
+          message: 'Please provide name and bio for the user',
+        })
+      } else {
+        const updatedUser = await User.update(req.params.id, req.body)
+        res.status(200).json(updatedUser)
+      }
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'error updating user',
       err: err.message,
       stack: err.stack,
     })
