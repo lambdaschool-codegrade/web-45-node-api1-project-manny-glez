9e33a7c29e32298d2ac4390b08740078b4252d39
Emmanuel Gonzalez <dev.egonzalez1@gmail.com>
2021-08-30 20:16:45 -0500

    Adds delete user endpoint

--- api/server.js
+++ api/server.js
@@ -5,8 +5,31 @@ const express = require('express')
 const User = require('./users/model')
 
 const server = express()
+server.use(express.json())
 
 // Endpoints
+// [POST] Creates a user using the information sent inside the request body.
+server.post('/api/users', (req, res) => {
+  const user = req.body
+  if (!user.name || !user.bio) {
+    res.status(400).json({
+      message: 'please provide name and bio for the user',
+    })
+  } else {
+    User.insert(user)
+      .then(createdUser => {
+        res.status(201).json(createdUser)
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: 'error creating user',
+          err: err.message,
+          stack: err.stack,
+        })
+      })
+  }
+})
+
 // [GET] Returns an array users.
 server.get('/api/users', (req, res) => {
   User.find()
@@ -16,7 +39,8 @@ server.get('/api/users', (req, res) => {
     .catch(err => {
       res.status(500).json({
         message: 'error getting users',
-        err: err.message
+        err: err.message,
+        stack: err.stack,
       })
     })
 })
@@ -40,13 +64,31 @@ server.get('/api/users/:id', (req, res) => {
     })
 })
 
+server.delete('/api/users/:id', async (req, res) => {
+  try {
+    const possibleUser = await User.findById(req.params.id)
+    if (!possibleUser) {
+      res.status(404).json({
+        message: 'The user with the right ID does not exist',
+      })
+    } else {
+      const deletedUser = await User.remove(possibleUser.id)
+      res.status(200).json(deletedUser)
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'error deleting user',
+      err: err.message,
+      stack: err.stack,
+    })
+  }
+})
+
 server.use('*', (req, res) => {
   res.status(404).json({
     message: 'not found'
   })
 })
-
-
 
 //module.exports = {}; // EXPORT YOUR SERVER instead of {}
 module.exports = server;
